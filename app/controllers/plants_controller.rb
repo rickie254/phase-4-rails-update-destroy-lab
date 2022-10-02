3b2e3bf2479635f462a824c482e0c62c6d75797b
rickie <rickiemaweu@gmail.com>
2022-10-02 15:40:35 +0300

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,49 @@
 class PlantsController < ApplicationController
-
-  # GET /plants
-  def index
-    plants = Plant.all
-    render json: plants
+  # rescue_from ActiveRecord::RecordNotFound, with::render_not_found_response
+    # GET /plants
+    def index
+      plants = Plant.all
+      render json: plants
+    end
+  
+    # GET /plants/:id
+    def show
+      plant = find_plant
+      render json: plant
+    end
+  
+    # POST /plants
+    def create
+      plant = Plant.create(plant_params)
+      render json: plant, status: :created
+    end
+  
+    # UPDATE /plants/:id
+    def update
+      plant=find_plant
+      plant.update(plant_params)
+      render json: plant
+    end
+  
+    # DELETE /plants/:id
+  
+    def destroy
+      plant = find_plant
+      plant.destroy
+      head:no_content
+    end
+  
+    private
+  
+    def find_plant
+      Plant.find(params[:id])
+    end
+  
+    def plant_params
+      params.permit(:name, :image, :price, :is_in_stock)
+    end
+  
+    def render_not_found_response
+      render json: {error:"plant not found"}, status: :not_found
+      end
   end
-
-  # GET /plants/:id
-  def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
-  end
-
-  # POST /plants
-  def create
-    plant = Plant.create(plant_params)
-    render json: plant, status: :created
-  end
-
-  private
-
-  def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
-  end
-end
